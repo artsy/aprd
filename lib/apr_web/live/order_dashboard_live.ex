d6aab8b1bc06054ad06584986ed42407e185d7f3
ashkan18 <ashkan18@gmail.com>
2019-05-18 09:03:15 -0400

    Remove pending approvals and show current active

--- lib/apr_web/live/order_dashboard_live.ex
+++ lib/apr_web/live/order_dashboard_live.ex
@@ -14,10 +14,10 @@ defmodule AprWeb.OrderDashboardLive do
           <%= currency(@approved_orders.totals.commission_cents) %>
         </palette-jumbo>
         <palette-jumbo label="Pending Approval GMV">
-          <%= currency(@pending_approvals.totals.amount_cents) %>
+          <%= currency(@active_orders.totals.amount_cents) %>
         </palette-jumbo>
         <palette-jumbo label="Pending Approval Commission">
-          <%= currency(@pending_approvals.totals.commission_cents) %>
+          <%= currency(@active_orders.totals.commission_cents) %>
         </palette-jumbo>
       </section>
       <section class="event">
@@ -54,24 +54,6 @@ defmodule AprWeb.OrderDashboardLive do
           </section>
         </section>
       <% end %>
-      <%= if !Enum.empty?(@pending_approvals.events) do %>
-        <section class="event">
-          <h2 class="sans-6"> Pending Approval </h2>
-          <section class="artworks">
-          <%= for event <- @pending_approvals.events do %>
-            <div class="artwork-event">
-              <% artwork = List.first(@artworks[event.payload["object"]["id"]]) %>
-              <img class="mb-1" src="<%= artwork["imageUrl"] %>" />
-              <div class="mb-0_5 sans-2-medium"> <%= currency(event.payload["properties"]["buyer_total_cents"]) %> </div>
-              <div class="serif-2-semibold color-black60"> <%= artwork["artist_names"] %> </div>
-              <div class="serif-2-italic color-black60"> <%= artwork["title"] %> </div>
-              <div class="serif-2 color-black60"> <%= artwork["partner"]["name"] %> </div>
-              <div class="serif-2 color-black30"> <a href="<%= exchange_link(event.payload["object"]["id"]) %>"> <%= event.payload["properties"]["code"] %></a></div>
-            </div>
-          <% end %>
-          </section>
-        </section>
-      <% end %>
     </div>
     """
   end
@@ -85,7 +67,6 @@ defmodule AprWeb.OrderDashboardLive do
     {:ok,
      assign(socket,
        approved_orders: %{events: [], totals: %{amount_cents: 0, commission_cents: 0}, count: 0},
-       pending_approvals: %{events: [], totals: %{amount_cents: 0, commission_cents: 0}, count: 0},
        active_orders: %{events: [], totals: %{amount_cents: 0, commission_cents: 0}, count: 0},
        artworks: %{}
      )}
@@ -97,15 +78,13 @@ defmodule AprWeb.OrderDashboardLive do
 
   defp repopulate(socket) do
     approved_order_events = Events.list_events(routing_key: "order.approved", day_threshold: 1)
-    pending_approval_orders = Events.pending_approval_orders()
     active_orders = Events.active_orders()
 
     with {:ok, artworks} <-
-           Events.fetch_artworks(approved_order_events ++ pending_approval_orders ++ active_orders) do
+           Events.fetch_artworks(approved_order_events ++ active_orders) do
       {:noreply,
        assign(socket,
          approved_orders: aggregated_data(approved_order_events),
-         pending_approvals: aggregated_data(pending_approval_orders),
          active_orders: aggregated_data(active_orders),
          artworks: artworks
        )}
