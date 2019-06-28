d42f701ad0a3aa0c19741b9f23e52df1d1777591
ashkan <ashkan@artsymail.com>
2019-06-28 13:41:41 -0400

    move some logic to gravity

--- lib/apr/gravity.ex
+++ lib/apr/gravity.ex
@@ -15,4 +15,14 @@ defmodule Gravity do
   def process_request_url(url) do
     Application.get_env(:apr, Gravity)[:api_url] <> url
   end
+
+  def match_partners(term, token) do
+    get!("/api/v1/match/partners", [{:"X-ACCESS-TOKEN", token}], params: %{term: term}).body
+    |> Enum.map(fn partner -> Map.take(partner, ["_id", "name"]) end)
+  end
+
+  def match_users(term, token) do
+    get!("/api/v1/match/users", [{:"X-ACCESS-TOKEN", token}], params: %{term: term}).body
+    |> Enum.map(fn partner -> Map.take(partner, ["_id", "name", "email"]) end)
+  end
 end
--- lib/apr_web/live/order_by_partner.ex
+++ lib/apr_web/live/order_by_partner.ex
@@ -2,33 +2,34 @@ defmodule AprWeb.OrderByPartner do
   use Phoenix.LiveView
   import Apr.ViewHelper
   @gravity_api Application.get_env(:apr, :gravity_api)
-  @exchange_url Application.get_env(:apr, :exchange)[:url]
 
   def render(assigns) do
     ~L"""
-    <form phx-change="suggest_partner" class="autocomplete" autocomplete="off">
-      <label for="term"> Find orders by Partner </label>
-      <input type="text" name="term" value="<%= @partner_query %>" list="partner_matches"
-            placeholder="Search for partners..."
-            <%= if @loading, do: "readonly" %> />
-      <%= for match <- @partner_matches do %>
-        <div class="autocompleteItem">
-          <a href="<%= exchange_partner_orders_link(match["_id"]) %>" target="_blank"> <%= match["name"] %></a>
-        </div>
-      <% end %>
-    </form>
+    <div class="autocomplete-section">
+      <form phx-change="suggest_partner" class="autocomplete" autocomplete="off">
+        <label for="term"> Find orders by Partner </label>
+        <input type="text" name="term" value="<%= @partner_query %>" list="partner_matches"
+              placeholder="Search for partners..."
+              <%= if @loading, do: "readonly" %> />
+        <%= for match <- @partner_matches do %>
+          <div class="autocompleteItem">
+            <a href="<%= exchange_partner_orders_link(match["_id"]) %>" target="_blank"> <%= match["name"] %></a>
+          </div>
+        <% end %>
+      </form>
 
-    <form phx-change="suggest_user" class="autocomplete" autocomplete="off">
-      <label for="term"> Find orders by User </label>
-      <input type="text" name="term" value="<%= @partner_query %>" list="user_matches"
-            placeholder="Search for user..."
-            <%= if @loading, do: "readonly" %> />
-      <%= for match <- @user_matches do %>
-        <div class="autocompleteItem">
-          <a href="<%= exchange_user_orders_link(match["_id"]) %>" target="_blank"> <%= match["name"] %> - <%= match["email"] %></a>
-        </div>
-      <% end %>
-    </form>
+      <form phx-change="suggest_user" class="autocomplete" autocomplete="off">
+        <label for="term"> Find orders by User </label>
+        <input type="text" name="term" value="<%= @partner_query %>" list="user_matches"
+              placeholder="Search for user..."
+              <%= if @loading, do: "readonly" %> />
+        <%= for match <- @user_matches do %>
+          <div class="autocompleteItem">
+            <a href="<%= exchange_user_orders_link(match["_id"]) %>" target="_blank"> <%= match["name"] %> - <%= match["email"] %></a>
+          </div>
+        <% end %>
+      </form>
+    </div>
     """
   end
 
@@ -45,35 +46,16 @@ defmodule AprWeb.OrderByPartner do
   end
 
   def handle_event("suggest_partner", %{"term" => term}, socket) when byte_size(term) >= 3 do
-    partner_matches = fetch_partners(term, socket.assigns.access_token)
+    partner_matches = @gravity_api.match_partners(term, socket.assigns.access_token)
     {:noreply, assign(socket, partner_matches: partner_matches)}
   end
 
   def handle_event("suggest_partner", _, socket), do: {:noreply, assign(socket, partner_matches: [])}
 
   def handle_event("suggest_user", %{"term" => term}, socket) when byte_size(term) >= 3 do
-    user_matches = fetch_users(term, socket.assigns.access_token)
+    user_matches = @gravity_api.match_users(term, socket.assigns.access_token)
     {:noreply, assign(socket, user_matches: user_matches)}
   end
 
   def handle_event("suggest_user", _, socket), do: {:noreply, assign(socket, user_matches: [])}
-
-  def handle_event("select_partner" <> partner_id, _, socket) do
-    {:stop,
-     socket
-     |> put_flash(:info, "Partner Selected")
-     |> redirect(
-       to: "#{@exchange_url}/admin/orders?q%5Bseller_id_eq=#{partner_id}"
-     )}
-  end
-
-  defp fetch_partners(term, token) do
-    @gravity_api.get!("/api/v1/match/partners", [{:"X-ACCESS-TOKEN", token}], params: %{term: term}).body
-    |> Enum.map(fn partner -> Map.take(partner, ["_id", "name"]) end)
-  end
-
-  defp fetch_users(term, token) do
-    @gravity_api.get!("/api/v1/match/users", [{:"X-ACCESS-TOKEN", token}], params: %{term: term}).body
-    |> Enum.map(fn partner -> Map.take(partner, ["_id", "name", "email"]) end)
-  end
 end
