ba94d0195a6917a6d28e9f2a0d0a1dc7c17ea723
ashkan <ashkan@artsymail.com>
2019-07-02 11:42:06 -0400

    Switch to method for exchange_url instead of module attribute

--- lib/apr_web/helpers/view_helper.ex
+++ lib/apr_web/helpers/view_helper.ex
@@ -1,9 +1,7 @@
 defmodule Apr.ViewHelper do
-  @exchange_url Application.get_env(:apr, :exchange)[:url]
-
   @spec exchange_link(String.t()) :: String.t()
   def exchange_link(order_id) do
-    "#{@exchange_url}/admin/orders/#{order_id}"
+    "#{exchange_url()}/admin/orders/#{order_id}"
   end
 
   @spec currency(integer, atom | binary) :: String.t()
@@ -15,13 +13,17 @@ defmodule Apr.ViewHelper do
 
   @spec exchange_user_orders_link(String.t()) :: String.t()
   def exchange_user_orders_link(user_id) do
-    "#{@exchange_url}/admin/orders?q[buyer_id_eq]=#{user_id}"
+    "#{exchange_url()}/admin/orders?q[buyer_id_eq]=#{user_id}"
     |> URI.encode
   end
 
   @spec exchange_partner_orders_link(String.t()) :: String.t()
   def exchange_partner_orders_link(partner_id) do
-    "#{@exchange_url}/admin/orders?q[seller_id_eq]=#{partner_id}"
+    "#{exchange_url()}/admin/orders?q[seller_id_eq]=#{partner_id}"
     |> URI.encode
+  end
+
+  defp exchange_url do
+    Application.get_env(:apr, :exchange)[:url]
   end
 end
