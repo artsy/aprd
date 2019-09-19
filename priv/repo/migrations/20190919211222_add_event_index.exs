ed87548e406a43fa44d45225057c191847a8cc41
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2019-09-19 17:17:10 -0400

    Add proper index

---
+++ priv/repo/migrations/20190919211222_add_event_index.exs
@@ -0,0 +1,11 @@
+defmodule Apr.Repo.Migrations.AddEventIndex do
+  use Ecto.Migration
+
+  def change do
+    create index(:events, [:routing_key], comment: "Index routing_key")
+    create index(:events, [:topic], comment: "Index topid")
+    create index(:events, [:inserted_at], comment: "Index inserted_at")
+    create index(:events, [:payload], using: :gin, comment: "Index payload")
+
+  end
+end
