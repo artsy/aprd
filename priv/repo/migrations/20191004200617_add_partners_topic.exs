de4e4cfc45939b6bb69e98dd93bac04e59dca98f
Matt Dole <mdole7@gmail.com>
2019-10-07 11:05:22 -0400

    remove partners topic migration

--- priv/repo/migrations/20191004200617_add_partners_topic.exs
+++
@@ -1,8 +0,0 @@
-defmodule Apr.Repo.Migrations.AddPartnersTopic do
-  use Ecto.Migration
-  alias Apr.Subscriptions
-
-  def change do
-    Subscriptions.create_topic(%{name: "partners"})
-  end
-end
