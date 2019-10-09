617bd088558689dc231c1b26d1fe5ddda32cdc37
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2019-10-09 15:32:35 -0400

    Better handling of getting liability shift flag

--- lib/apr/behaviours/payments_behaviour.ex
+++ lib/apr/behaviours/payments_behaviour.ex
@@ -1,3 +1,3 @@
 defmodule Apr.PaymentsBehaviour do
-  @callback liability_shift_happened(string()) :: boolean()
+  @callback liability_shift_happened(String.t()) :: boolean()
 end
--- lib/apr/payments.ex
+++ lib/apr/payments.ex
@@ -4,11 +4,13 @@ defmodule Apr.Payments do
   alias Stripe.PaymentIntent
 
   def liability_shift_happened(external_charge_id) do
-    with {:ok, pi} <- PaymentIntent.retrieve(external_charge_id, %{expand: ["payment_method"]}),
-         charge <- List.first(pi.charges) do
-      charge.payment_method_details.card.three_d_secure.succeeded
+    with {:ok, pi} <- PaymentIntent.retrieve(external_charge_id, %{}),
+         [charge | _tail] <- pi.charges.data,
+         %{three_d_secure: three_d_secure} when is_map(three_d_secure) <- charge.payment_method_details.card,
+         %{succeeded: succeeded} <- three_d_secure do
+      succeeded
     else
-      _ -> nil
+      _ -> false
     end
   end
 end
