93a5b9a74ded9159c0553146b2a973096b637d22
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2019-12-18 13:41:46 -0500

    Not show offer events for Fraud theme

--- lib/apr/views/commerce/commerce_order_slack_view.ex
+++ lib/apr/views/commerce/commerce_order_slack_view.ex
@@ -10,10 +10,10 @@ defmodule Apr.Views.CommerceOrderSlackView do
 
   def render(
         %Subscription{theme: "fraud"},
-        %{"verb" => verb, "properties" => %{"items_total_cents" => items_total_cents}},
+        %{"verb" => verb, "properties" => %{"items_total_cents" => items_total_cents, "mode" => mode}},
         _routing_key
       )
-      when items_total_cents < 3_000_00 or verb != "submitted",
+      when items_total_cents < 3_000_00 or verb != "submitted" or mode == "offer",
       do: nil
 
   def render(_subscription, event, routing_key) do
