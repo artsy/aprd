31b70a47b6c2143812f75ca8a15281163323e78c
Kazunari Okuda <saori.cube@gmail.com>
2022-08-14 10:58:29 +0200

    feat: create a new view for the slack notification

---
+++ lib/apr/views/ach_slack_view.ex
@@ -0,0 +1,40 @@
+defmodule Apr.Views.ACHSlackView do
+  import Apr.Views.Helper
+
+  def render(subscription, event = %{"verb" => "dispute_created",}, _) do
+    case {subscription.theme, event["verb"], event["properties"]} do
+      {"ach", "dispute_created", %{"payment_method" => "us_bank_account"}} ->
+        generate_slack_message(event, "dispute_created")
+      _ -> nil
+    end
+  end
+
+  def render(_, _, _), do: nil
+
+  defp generate_slack_message(event, _routing_key) do
+    order_id = event["object"]["id"]
+    seller_id = event["properties"]["seller_id"]
+    seller_path = "partner/#{seller_id}"
+
+    %{
+      text: ":alert: Dispute, do not refund this order on Stripe",
+      attachments: [
+        %{
+          fields: [
+            %{
+              title: "Order ID",
+              value: "<#{exchange_admin_link(order_id)}|#{order_id}>",
+              short: true
+            },
+            %{
+              title: "Seller ID",
+              value: "<#{admin_partners_link(seller_path)}|#{seller_id}>",
+              short: true
+            }
+          ]
+        }
+      ],
+      unfurl_links: true
+    }
+  end
+end
---
+++ test/apr/views/ach_slack_view_test.exs
@@ -0,0 +1,40 @@
+defmodule Apr.Views.ACHSlackViewTest do
+  use ExUnit.Case, async: true
+  alias Apr.Views.ACHSlackView
+  alias Apr.Fixtures
+  alias Apr.Subscriptions.Subscription
+
+  @subscription %Subscription{theme: "ach"}
+
+  test "disputed payment" do
+    event = Fixtures.commerce_order_event(
+      "dispute_created",
+      %{
+        "payment_method" => "us_bank_account",
+        "tax_transaction_id" => "txn_1"
+      }
+    )
+    slack_view = ACHSlackView.render(@subscription, event, "order.submitted")
+
+    assert slack_view == %{
+      attachments: [
+        %{
+          fields: [
+            %{
+              short: true,
+              title: "Order ID",
+              value: "<https://exchange.artsy.net/admin/orders/order123|order123>"
+            },
+            %{
+              short: true,
+              title: "Seller ID",
+              value: "<https://admin-partners.artsy.net/partner/partner1|partner1>"
+            }
+          ]
+        }
+      ],
+      text: ":alert: Dispute, do not refund this order on Stripe",
+      unfurl_links: true
+    }
+  end
+end
