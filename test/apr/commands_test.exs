59b53e8a12a71d2bbc87e47beb8aed223c504e92
Kazunari Okuda <saori.cube@gmail.com>
2022-08-31 12:08:18 +0200

    test: add a test case for Commands.process_command with the `subscribe` command

--- test/apr/commands_test.exs
+++ test/apr/commands_test.exs
@@ -2,19 +2,30 @@ defmodule Apr.CommandsTest do
   use Apr.DataCase
 
   describe ".process_command" do
+    alias Apr.Repo
     alias Apr.Commands
+    alias Apr.Subscriptions
+    alias Apr.Subscriptions.{Subscription}
 
-    test "subscriptions command" do
+    test "when the command is subscribe" do
       params = %{
         "channel_id" => "1",
         "team_id" => "1",
         "team_domain" => "test",
-        "text" => "subscriptions",
+        "text" => "subscribe commerce:order.approved->high_risk",
         "channel_name" => "test",
         "user_id" => "1",
         "user_name" => "test"
       }
-      assert Commands.process_command(params) == %{response_type: "in_channel", text: "Subscribed topics: \n "}
+
+      {:ok, subscriber} = Subscriptions.create_subscriber(params)
+      {:ok, topic} = Subscriptions.create_topic(%{name: "commerce"})
+
+      assert Commands.process_command(params) == %{response_type: "in_channel", text: ":+1: Subscribed to *commerce*:order.approved->high_risk"}
+
+      subscription = Repo.get_by(Subscription, subscriber_id: subscriber.id, topic_id: topic.id)
+      assert subscription.routing_key == "order.approved"
+      assert subscription.theme == "high_risk"
     end
   end
 end
