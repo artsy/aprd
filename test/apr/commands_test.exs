ca110b23efe5051fe8d5d972757c856773fc9c16
Kazunari Okuda <saori.cube@gmail.com>
2022-09-14 16:38:14 +0200

    test: add a test for Commands.process_command to check how to subscribe all of the routing_key with a theme

--- test/apr/commands_test.exs
+++ test/apr/commands_test.exs
@@ -7,8 +7,8 @@ defmodule Apr.CommandsTest do
     alias Apr.Subscriptions
     alias Apr.Subscriptions.{Subscription}
 
-    test "when the command is subscribe" do
-      params = %{
+    test "when the command is `subscribe commerce:order.approved->high_risk`" do
+      subscriber_attrs = %{
         "channel_id" => "1",
         "team_id" => "1",
         "team_domain" => "test",
@@ -17,14 +17,33 @@ defmodule Apr.CommandsTest do
         "user_id" => "1",
         "user_name" => "test"
       }
-
-      {:ok, subscriber} = Subscriptions.create_subscriber(params)
+      {:ok, subscriber} = Subscriptions.create_subscriber(subscriber_attrs)
       {:ok, topic} = Subscriptions.create_topic(%{name: "commerce"})
 
-      assert Commands.process_command(params) == %{response_type: "in_channel", text: ":+1: Subscribed to *commerce*:order.approved->high_risk"}
+      assert Commands.process_command(subscriber_attrs) == %{response_type: "in_channel", text: ":+1: Subscribed to *commerce*:order.approved->high_risk"}
 
       subscription = Repo.get_by(Subscription, subscriber_id: subscriber.id, topic_id: topic.id)
       assert subscription.routing_key == "order.approved"
+      assert subscription.theme == "high_risk"
+    end
+
+    test "when the command is `subscribe commerce->high_risk`" do
+      subscriber_attrs = %{
+        "channel_id" => "1",
+        "team_id" => "1",
+        "team_domain" => "test",
+        "text" => "subscribe commerce->high_risk",
+        "channel_name" => "test",
+        "user_id" => "1",
+        "user_name" => "test"
+      }
+      {:ok, subscriber} = Subscriptions.create_subscriber(subscriber_attrs)
+      {:ok, topic} = Subscriptions.create_topic(%{name: "commerce"})
+
+      assert Commands.process_command(subscriber_attrs) == %{response_type: "in_channel", text: ":+1: Subscribed to *commerce*:#->high_risk"}
+
+      subscription = Repo.get_by(Subscription, subscriber_id: subscriber.id, topic_id: topic.id)
+      assert subscription.routing_key == "#"
       assert subscription.theme == "high_risk"
     end
   end
