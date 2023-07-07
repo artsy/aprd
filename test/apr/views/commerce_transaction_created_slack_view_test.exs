90e4119a6bb9f96926fab6b4a0e88373b14b0252
Chung-Yi Chi <chung-yi@artsymail.com>
2023-07-07 16:52:13 -0400

    feat: notify disputes for non-credit card orders

--- test/apr/views/commerce_transaction_created_slack_view_test.exs
+++ test/apr/views/commerce_transaction_created_slack_view_test.exs
@@ -7,12 +7,12 @@ defmodule Apr.Views.CommerceTransactionCreatedSlackViewTest do
   @subscription %Subscription{theme: "dispute"}
 
   describe "render/2" do
-    test "disputed payment" do
+    test "does not generate message for credit card disputes" do
       event = Fixtures.commerce_transaction_event(
         %{
           "id" => "order123",
           "seller_id" => "partner1",
-          "payment_method" => "us_bank_account",
+          "payment_method" => "credit card",
         },
         %{
           "verb" => "created",
@@ -22,32 +22,52 @@ defmodule Apr.Views.CommerceTransactionCreatedSlackViewTest do
         }
       )
       slack_view = CommerceTransactionCreatedSlackView.render(@subscription, event, "dispute")
+      assert is_nil(slack_view)
+    end
 
-      assert slack_view == %{
-        attachments: [
+    for payment_method <- ["us_bank_account", "sepa_debit"] do
+      test "generates message for #{payment_method} disputes" do
+        event = Fixtures.commerce_transaction_event(
           %{
-            fields: [
-              %{
-                short: true,
-                title: "Order ID",
-                value: "<https://exchange.artsy.net/admin/orders/order123|order123>"
-              },
-              %{
-                short: true,
-                title: "Seller ID",
-                value: "<https://admin-partners.artsy.net/partners/partner1|partner1>"
-              },
-              %{
-                short: true,
-                title: "Stripe payment ID",
-                value: "<https://dashboard.stripe.com/payments/pi_123|pi_123>"
-              }
-            ]
+            "id" => "order123",
+            "seller_id" => "partner1",
+            "payment_method" => unquote(payment_method),
+          },
+          %{
+            "verb" => "created",
+            "transaction_type" => "dispute",
+            "external_id" => "pi_123",
+            "external_type" => "payment_intent",
           }
-        ],
-        text: ":alert: Dispute, do not refund this order on Stripe",
-        unfurl_links: true
-      }
+        )
+        slack_view = CommerceTransactionCreatedSlackView.render(@subscription, event, "dispute")
+
+        assert slack_view == %{
+          attachments: [
+            %{
+              fields: [
+                %{
+                  short: true,
+                  title: "Order ID",
+                  value: "<https://exchange.artsy.net/admin/orders/order123|order123>"
+                },
+                %{
+                  short: true,
+                  title: "Seller ID",
+                  value: "<https://admin-partners.artsy.net/partners/partner1|partner1>"
+                },
+                %{
+                  short: true,
+                  title: "Stripe payment ID",
+                  value: "<https://dashboard.stripe.com/payments/pi_123|pi_123>"
+                }
+              ]
+            }
+          ],
+          text: ":alert: Dispute, do not refund this order on Stripe",
+          unfurl_links: true
+        }
+      end
     end
   end
 end
