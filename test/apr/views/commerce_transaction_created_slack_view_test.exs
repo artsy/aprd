d3ed58ac680ecef6d049d159102b3b97af87a4fd
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2023-07-10 10:25:06 -0400

    Allow exchange URL to be pulled from environment vars (#435)

--- test/apr/views/commerce_transaction_created_slack_view_test.exs
+++ test/apr/views/commerce_transaction_created_slack_view_test.exs
@@ -30,7 +30,7 @@ defmodule Apr.Views.CommerceTransactionCreatedSlackViewTest do
               %{
                 short: true,
                 title: "Order ID",
-                value: "<https://exchange.artsy.net/admin/orders/order123|order123>"
+                value: "<https://exchange-staging.artsy.net/admin/orders/order123|order123>"
               },
               %{
                 short: true,
